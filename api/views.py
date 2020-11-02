019231aa83d107e7ac78af89214279e0900c25dc
Mohamed Abdultawab <mohamedtoba96@gmail.com>
2020-11-02 12:59:30 +0200

    Documenting `views`.

--- api/views.py
+++ api/views.py
@@ -1,6 +1,23 @@
 from django.http import JsonResponse
+from django.http.response import HttpResponseNotFound, HttpResponseServerError
 from .utils import get_languages
 
 # Create your views here.
 def list_100_languages(request):
-    return JsonResponse(get_languages())
+    """API endpoint to list the languages used by the 100 trending public repos on GitHub.
+
+    Args:
+        request (WSGIRequest): django incoming request
+
+    Returns:
+        JsonResponse:           Languages used by the 100 trending public repos on GitHub.
+        [HttpResponseNotFound]: [if request is not GET]
+    """
+    if not request.method == "GET":
+        return HttpResponseNotFound()
+
+    try:
+        data = get_languages()
+        return JsonResponse(data)
+    except:
+        return HttpResponseServerError
